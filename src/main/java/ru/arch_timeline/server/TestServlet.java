823392ce1d158c84374d5618c6116ddb796732e1
sermolaev1986 <sermolaev.1986@gmail.com>
2013-08-20 22:46:52 +0600

    get and post implementation of test servlet.

--- src/main/java/ru/arch_timeline/server/TestServlet.java
+++ src/main/java/ru/arch_timeline/server/TestServlet.java
@@ -1,11 +1,45 @@
 package ru.arch_timeline.server;
 
-/**
- * Created with IntelliJ IDEA.
- * User: Сергей
- * Date: 20.08.13
- * Time: 22:01
- * To change this template use File | Settings | File Templates.
- */
-public class TestServlet {
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.gson.Gson;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+
+public class TestServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Gson obj = new Gson();
+
+        resp.setContentType("text/plain");
+        resp.setHeader("Cache-Control", "no-cache");
+
+        PrintWriter pw = resp.getWriter();
+        pw.write ( obj.toJson("hello world")) ;
+
+        pw.flush();
+        pw.close();
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        DatastoreService service =  DatastoreServiceFactory.getDatastoreService();
+        Entity entity = new Entity("ArchEvent");
+        entity.setProperty("eventDate", new Date());
+        entity.setProperty("eventTitle", "myTitle");
+
+        service.put(entity);
+    }
 }
