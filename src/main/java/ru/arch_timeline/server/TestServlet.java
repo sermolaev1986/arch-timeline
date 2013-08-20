0039bd561738a868df8de75f0cad1448015c4b65
sermolaev1986 <sermolaev.1986@gmail.com>
2013-08-20 22:32:27 +0600

    first test servlet. spits out hello world using json

---
+++ src/main/java/ru/arch_timeline/server/TestServlet.java
@@ -0,0 +1,11 @@
+package ru.arch_timeline.server;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Сергей
+ * Date: 20.08.13
+ * Time: 22:01
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestServlet {
+}
