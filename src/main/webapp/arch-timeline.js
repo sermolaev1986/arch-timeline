3c2e68426ccc033722b81a56eedfa2a5d932fc45
Ivan Nikolaev <ivan.nikolaeff@yandex.ru>
2013-09-02 22:54:46 +0600

    added gauge (canvas + kinetic js)

--- src/main/webapp/arch-timeline.js
+++ src/main/webapp/arch-timeline.js
@@ -1,9 +1,24 @@
-$("#btnGetData").click(function () {
+function refreshDataTable() {
+    var wait = $("#wait");
+    var dataTable = $("#dataTable");
+    var targetTableBody = $('#targetTableBody');
+    dataTable.addClass("hide");
+    wait.removeClass("hide");
     $.getJSON('hello-world', function (data) {
         var items = [];
         $.each(data, function () {
             items.push('<tr><td>' + this.eventTitle + '</td><td>' + this.eventDate + '</td></tr>');
         });
-        $('#targetTableBody').html(items.join(''))
+        targetTableBody.html(items.join(''));
+        wait.addClass("hide");
+        dataTable.removeClass("hide");
     });
+}
+$("#btnGetData").click(function () {
+    refreshDataTable();
+});
+
+$("#addBtn").click(function () {
+    $.ajax('hello-world');
+    refreshDataTable();
 });
